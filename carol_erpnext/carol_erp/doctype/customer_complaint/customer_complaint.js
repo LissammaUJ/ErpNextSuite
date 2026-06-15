27979ba2c716f28062110ba2e44bbc905747fa47
root <root@server.localhost.com>
2026-06-15 09:48:07 +0000

    Add Customer Complaint DocType

---
+++ carol_erpnext/carol_erp/doctype/customer_complaint/customer_complaint.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Carol Solutions and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Customer Complaint", {
+// 	refresh(frm) {
+
+// 	},
+// });
